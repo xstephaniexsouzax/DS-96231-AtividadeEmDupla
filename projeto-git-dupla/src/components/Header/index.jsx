7c6728c0372668d4d5dc6fd9998d4c3bf80ef004
queilaveloso-sol <queila.veloso@ba.estudante.senai.br>
2026-06-10 19:37:56 -0300

    Melhoria no espaçamento e estilo do Header

--- projeto-git-dupla/src/components/Header/index.jsx
+++ projeto-git-dupla/src/components/Header/index.jsx
@@ -2,18 +2,19 @@ import { Link } from 'react-router-dom';
 import './style.css';
 
 function Header() {
-	return (
-		<header className="header-container">
-			<div className="header-logo">
-				<h2>Psicologia <span>Online</span></h2>
-			</div>
-			<nav className="header-nav">
-				<Link to="/" className="nav-link">Home</Link>
-				<Link to="/produtos" className="nav-link">Produtos</Link>
-				<Link to="/servicos" className="nav-link">Serviços</Link>
-			</nav>
-		</header>
-	);
+  return (
+    <header className="header-container">
+      <div className="header-logo">
+        <h2>Psicologia <span>Online</span></h2>
+      </div>
+      
+      <nav className="menu-navegacao">
+        <Link to="/">Home</Link>
+        <Link to="/produtos">Produtos</Link>
+        <Link to="/servicos">Serviços</Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
